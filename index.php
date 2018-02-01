8b4852fb9c60df05f3b1258afcc9f7b7afc78742
Vinícius Schlee Tessmann <viniciusschleetessmann@gmail.com>
2018-02-01 00:01:26 -0200

    Create file to receive mail

--- index.php
+++ index.php
@@ -458,25 +458,25 @@ http://www.tooplate.com/view/2089-meteor
                 </div>
                 <div class="col-md-6">
                     <div class="row">
-                      <form id="contact" action="" method="post">
+                      <form id="contact" action="mailer.php" method="post">
                         <div class="col-md-6">
                           <fieldset>
-                            <input name="name" type="text" class="form-control" id="name" placeholder="Your name..." required="">
+                            <input name="name" type="text" class="form-control" id="name" placeholder="Seu nome..." required="">
                           </fieldset>
                         </div>
                         <div class="col-md-6">
                           <fieldset>
-                            <input name="email" type="email" class="form-control" id="email" placeholder="Your email..." required="">
+                            <input name="email" type="email" class="form-control" id="email" placeholder="Seu email..." required="">
                           </fieldset>
                         </div>
                         <div class="col-md-12">
                           <fieldset>
-                            <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your message..." required=""></textarea>
+                            <textarea name="message" rows="6" class="form-control" id="message" placeholder="Sua mensagem..." required=""></textarea>
                           </fieldset>
                         </div>
                         <div class="col-md-12">
                           <fieldset>
-                            <button type="submit" id="form-submit" class="btn">Send Message</button>
+                            <button type="submit" id="form-submit" class="btn">Enviar</button>
                           </fieldset>
                         </div>
                       </form>
---
+++ mailer.php
@@ -0,0 +1,20 @@
+<?php
+
+  $emailenviar = "contato@viniciustessmann.web6213.kinghost.net";
+  $destino = 'viniciusschleetessmann@gmail.com';
+  $assunto = "Contato pelo Site";
+
+  // É necessário indicar que o formato do e-mail é html
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+      $headers .= 'From: $nome <$email>';
+  //$headers .= "Bcc: $EmailPadrao\r\n";
+  
+  $enviaremail = mail($destino, $assunto, $headers);
+  if($enviaremail){
+    $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+    echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
+    } else {
+    $mgm = "ERRO AO ENVIAR E-MAIL!";
+    echo "";
+    }
